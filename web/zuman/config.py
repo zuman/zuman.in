2e4b4315f7ba5f8e0e57cf7011f3e3b3e3087b61
zuman.mac <syed.zuman.007@gmail.com>
2022-04-18 00:38:08 +0530

    Optimize env var delivery

--- web/zuman/config.py
+++ web/zuman/config.py
@@ -1,5 +1,4 @@
 
-import json
 import os
 
 import memcache
@@ -8,33 +7,31 @@ from zuman import constants
 
 conf = {}
 if os.getenv('FLASK_APP') == constants.FLASK_APP_DEV:
-    conf['CONFIG_FILE'] = os.getcwd() + "/web/config/conf.json"
+    conf['SESSION_MEMCACHED'] = "localhost:11211"
 else:
-    conf['CONFIG_FILE'] = "/var/zuman.one/conf.json"
-
-with open(conf['CONFIG_FILE']) as config_file:
-    conf = json.load(config_file)
-
-    if os.getenv('FLASK_APP') == constants.FLASK_APP_DEV:
-        conf['SESSION_MEMCACHED'] = "localhost:11211"
-    conf["SQLALCHEMY_DATABASE_URI"] = conf["SQLALCHEMY_DATABASE_URI"].replace("DB_PASS", os.getenv('DB_PASS'))
+    conf['SESSION_MEMCACHED'] = os.getenv('SESSION_MEMCACHED')
+conf["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI").replace("DB_PASS", os.getenv('POSTGRES_PASSWORD'))
 
 
 class Config:
-    SECRET_KEY = conf["SECRET_KEY"]
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    LOG_LEVEL = os.getenv("LOG_LEVEL")
+
     SQLALCHEMY_DATABASE_URI = conf["SQLALCHEMY_DATABASE_URI"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SESSION_TYPE = os.getenv('SESSION_TYPE')
+    SESSION_MEMCACHED = memcache.Client([conf['SESSION_MEMCACHED']])
+    SESSION_COOKIE_NAME = os.getenv('SESSION_COOKIE_NAME')
+    SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE') == 'True'
+    PERMANENT_SESSION_LIFETIME = int(os.getenv('PERMANENT_SESSION_LIFETIME'))
+
+    REMEMBER_COOKIE_DURATION = int(os.getenv('REMEMBER_COOKIE_DURATION'))
+    REMEMBER_COOKIE_HTTPONLY = os.getenv('REMEMBER_COOKIE_HTTPONLY') == 'True'
+    REMEMBER_COOKIE_SECURE = os.getenv('REMEMBER_COOKIE_SECURE') == 'True'
+
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = conf['MAIL_USERNAME']
-    MAIL_PASSWORD = conf['MAIL_PASSWORD']
-    LOG_LEVEL = conf['LOG_LEVEL']
-    SESSION_TYPE = conf['SESSION_TYPE']
-    SESSION_MEMCACHED = memcache.Client([conf['SESSION_MEMCACHED']])
-    SESSION_COOKIE_NAME = conf['SESSION_COOKIE_NAME']
-    SESSION_COOKIE_SECURE = conf['SESSION_COOKIE_SECURE']
-    PERMANENT_SESSION_LIFETIME = conf['PERMANENT_SESSION_LIFETIME']
-    REMEMBER_COOKIE_DURATION = conf['REMEMBER_COOKIE_DURATION']
-    REMEMBER_COOKIE_HTTPONLY = conf['REMEMBER_COOKIE_HTTPONLY']
-    REMEMBER_COOKIE_SECURE = conf['REMEMBER_COOKIE_SECURE']
